2344a1cb3ddb5dc7dd97ceb525ad00c99d626dd7
Alastair Longmuir <alastairlinwood@gmail.com>
2024-04-25 13:18:07 +0200

    instructor card css

--- src/components/InstructorCard.jsx
+++ src/components/InstructorCard.jsx
@@ -4,16 +4,17 @@ const InstructorCard = ({ instructor }) => {
   return (
     <Link to={`/instructor/${instructor._id}`}
       key={instructor.id}
-      className="card bordered rounded-2xl m-4 p-0 w-64 flex-col justify-center items-center bg-slate-200"
+      className="card bordered rounded-3xl m-4 p-0 w-64 flex-col justify-center items-center bg-slate-100"
     >
       <figure>
         <img src={instructor.picture} alt="Profile picture" className="w-64 h-64 object-cover grayscale"/>
       </figure>
-      <div className="card-body flex flex-col w-full">
+      <div className="card-body flex flex-row justify-between items-center w-full">
+        
         <h3 className="card-title text-primary">
           {instructor.name.first}
         </h3>
-        <div className="flex justify-start gap-2">{instructor.classes.map(course => <span className="badge badge-neutral bg-white border-none text-primary rounded-xl">{course}</span>)}</div>
+        <div className="flex justify-start gap-2">{instructor.classes.slice(0,1).map(course => <span className="badge badge-sm badge-outline badge-accent text-primary rounded-3xl p-3">{course}</span>)}</div>
         
       </div>
     </Link>
